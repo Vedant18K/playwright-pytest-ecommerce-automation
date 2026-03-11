b3c0e86d6be52fe096c4cd5707f9025d518ea107
VedantInferface <vedanbkulkarni@gmail.com>
2026-03-11 15:44:21 +0530

    Code formator

--- tests/test_register_user.py
+++ tests/test_register_user.py
@@ -1,25 +1,33 @@
-
 from pages.register_page import RegisterPage
 from utils.logger import logger
 from utils.data_reader import load_test_data
-from utils.data_generator import generate_email , generate_mobile
+from utils.data_generator import generate_email, generate_mobile, generate_password
+
 
 def test_register_user(page):
     data = load_test_data("data/register_data.json")
     register = RegisterPage(page)
     email = generate_email()
-    print(email)
     mobile = generate_mobile()
-    print(mobile)
+    password = generate_password()
+    print(email,mobile,password)
     logger.info("Sign up initiated")
     register.navigate_home()
     register.click_signup_login()
-    register.signup_user(data["name"],email)
+    register.signup_user(data["name"], email)
     logger.info("Account Information initiated")
-    register.account_information(data["password"])
+    register.account_information(password)
     logger.info("Address Information Initiated")
-    register.address_information(data["first_name"],data["last_name"],data["company"],data["address"],data["address2"],data["state"],data["city"],data["zipcode"],mobile)
-    logger.info("Account Created")
+    register.address_information(
+        data["first_name"],
+        data["last_name"],
+        data["company"],
+        data["address"],
+        data["address2"],
+        data["state"],
+        data["city"],
+        data["zipcode"],
+        mobile,
+    )
     register.create_account()
     logger.info("User registered successfully")
-    
--- utils/data_generator.py
+++ utils/data_generator.py
@@ -5,3 +5,6 @@ def generate_email():
 
 def generate_mobile():
     return f"9{str(int(time.time()*1000))[-9:]}"
+
+def generate_password():
+    return f"Sam@{str(int(time.time()*1000))[-4:]}ple"
